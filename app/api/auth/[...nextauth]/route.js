c8193bb6a4243e626b7a519f86b06581b18cef5e
Vinod-Mendis <136561979+Vinod-Mendis@users.noreply.github.com>
2024-06-09 13:12:05 +0700

    Correctly Hooked the Database
    Created a callback

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -18,44 +18,47 @@ const handler = NextAuth( {
       clientSecret: process.env.GOOGLE_CLIENT_SECRET
     })
   ],
-
-  // User session
-  async session({session}) {
-    const userSession = await User.findOne({
-      email: session.user.email
-    })
-
-    session.user.id = sessionUser._id.toString();
-    
-    return session;
-  },
-
-  async signIn ({profile}) {
-    try {
-      // serveless -> lambda
-      await connectToDB();
-
-      // check if a user already exists
-      const userExists = await User.findOne({
-        email: profile.email
+  callbacks: {
+    // User session
+    async session({session}) {
+      const sessionUser = await User.findOne({
+        email: session.user.email
       })
 
-      // if not, create a new user and save it to the database
-      if (!userExists) {
-        await User.create({
-          email: profile.email,
-          username: profile.name.replace(" ","").toLowercase(),
-          image: profile.picture
-        })
-        
-      }
+      session.user.id = sessionUser._id.toString();
+      
+      return session;
+    },
 
-      return true;
-    } catch (error) {
-      console.log(error);
-      return false;
+    async signIn ({profile}) {
+      try {
+        // serveless -> lambda
+        await connectToDB();
+
+        // check if a user already exists
+        const userExists = await User.findOne({
+          email: profile.email
+        })
+
+        // if not, create a new user and save it to the database
+        if (!userExists) {
+          await User.create({
+            email: profile.email,
+            username: profile.name.replace(" ","").toLowerCase(),
+            image: profile.picture
+          })
+          
+        }
+
+        return true;
+      } catch (error) {
+        console.log(error);
+        return false;
+      }
     }
   }
+
+  
 })
 
 export { handler as GET, handler as POST};
--- utils/database.js
+++ utils/database.js
@@ -1,30 +1,26 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
 let isConnected = false; // track the connection
 
-export const connectToDB = async() => {
+export const connectToDB = async () => {
   mongoose.set('strictQuery', true);
 
   if(isConnected) {
-    console.log("MongoDB is already Connected");
+    console.log('MongoDB is already connected');
     return;
   }
 
   try {
     await mongoose.connect(process.env.MONGODB_URI, {
       dbName: "share_prompt",
-      userNewUrlParser: true,
+      useNewUrlParser: true,
       useUnifiedTopology: true,
-
     })
 
     isConnected = true;
 
-    console.log("MongoDB connected");
-    
+    console.log('MongoDB connected')
   } catch (error) {
-
     console.log(error);
-    
   }
 }
